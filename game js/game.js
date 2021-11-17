a1a1b3e34ac1df69d42bb814bb8a9fa252a5e1d7
Emil-Ibraimov <92825360+Emil-Ibraimov@users.noreply.github.com>
2021-11-17 20:52:37 +0300

    fix game.js

--- game js/game.js
+++ game js/game.js
@@ -1,14 +1,41 @@
-const thievesArmy = (arrResurs, loadCapacity) =>{
-    let sum = 0
-    let arrLooted = []
-    arrResurs.forEach(function(item) {
-        sum += item
-      })
-      arrResurs.forEach(function(item) {
-        arrLooted.push(Math.floor(loadCapacity / (sum / item)))
-      })
-    return arrLooted
+const thievesArmy = (arrResurs, loadCapacity) => {
+  let sum = 0
+  let arrLooted = []
+  let coefficients = [];
+  let tempCoeff = []
+
+  arrResurs.forEach(function (item) {
+    sum += item
+  })
+
+  arrResurs.forEach(function (item) {
+    index = sum / item
+    coefficients.push(Math.round(index))
+    tempCoeff.push(Math.round(index))
+    arrLooted.push(0)
+  })
+
+  let id = 0;
+  tempCoeff.sort((a, b) => a - b)
+  let maxSum = 0;
+
+  arrResurs.some(function (item, index) {
+    id = coefficients.indexOf(tempCoeff[index])
+    arrLooted[id] = Math.ceil(loadCapacity / coefficients[id])
+    coefficients[id] = 0
+    maxSum += arrLooted[id]
+
+    if (maxSum == loadCapacity) {
+      return 1
+    }
+  })
+
+  return arrLooted
 }
 
-// let arr = [100, 300, 200]
-// console.log(thievesArmy(arr, 120))
+//let arr = [100, 300, 199]
+//let arr = [1,1,1,4,1,6] // 2
+//let arr = [1, 1, 1] // 2
+//let arr = [100, 300, 199] // 120
+//let arr = [1, 1, 6] //2
+//console.log(thievesArmy(arr, 120))
