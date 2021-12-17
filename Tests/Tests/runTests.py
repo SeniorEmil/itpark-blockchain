c5b955ae63427bf7ed2c89832acf6d91702dcf21
Emil <emil2002.04.02@gmail.com>
2021-12-17 15:51:22 +0300

    add tests

--- Tests/Tests/runTests.py
+++ Tests/Tests/runTests.py
@@ -9,6 +9,17 @@ def test1():
     b_num = 4
     clc.call_method('set_number', {'a': a_num, 'b': b_num})
     # Call a getter and ensure that we received correct integer value
-    assert eq(4, clc.call_getter('result'))
+    assert eq(6, clc.call_getter('result'))
+
+def test2():
+    # Deploy a contract to virtual blockchain
+    clc = ts4.BaseContract('calc', {})
+    a_num = 1000
+    b_num = 1000
+    clc.call_method('set_number', {'a': a_num, 'b': b_num})
+    # Call a getter and ensure that we received correct integer value
+    assert eq(2000, clc.call_getter('result'))
+
 
 test1()
+test2()
